8ee4e1934e53bdd4c32f1ac504d4fbe5efb3538d
Mateo Alganaras <tutealga@gmail.com>
2022-06-17 17:47:59 -0300

    Se agrego usuarios

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+let usuario = prompt('Como es tu nombre: ')
 let monto = parseInt(prompt('Cuanto dinero queres pedir prestado: '))
 let cuantasCuotas = parseInt(prompt('En cuantas cuotas queres devolver el prestamo (min: 1 - max: 12): '))
 
@@ -8,17 +9,32 @@ const existe = cuotas.find((cuota) => {
  })
 
 class Prestamo{
-    constructor(monto, cuotas){
+    constructor(monto, cuotas, usuario){
         this.monto = monto;
-        this.cuota = cuotas; 
+        this.cuotas = cuotas; 
+        this.usuario = usuario;
     }
-    calcular(monto, cuotas){
+    calcular(monto, cuotas, usuario){
         let resultado = (monto + (monto * cuotas * 0.12)) / cuotas
         let total = monto + (monto * cuotas * 0.12)
-        alert('Vas a devolver ' + cuotas + ' cuotas de ' + resultado)
-        alert('En total devolveras ' + total)
+        return prestamo.mostrarInfo(resultado, total, usuario)
+    }
+    mostrarInfo(resultado, total, usuario){
+        alert(usuario.nombre + ' tendras que devolver ' + this.cuotas + ' cuotas de ' + resultado)
+        alert(usuario.nombre + ' en total devolveras ' + total)
     }
 }
 
-const prestamo = new Prestamo(monto, existe)
-prestamo.calcular(monto, existe)
+class Usuario{
+    constructor(nombre, prestamos, cuotas){
+        this.nombre = nombre;
+        this.prestamos = prestamos;
+        this.cuotas = cuotas;
+    }
+}
+
+const nuevoUsuario = new Usuario(usuario, monto, existe)
+
+const prestamo = new Prestamo(monto, existe, nuevoUsuario)
+prestamo.calcular(monto, existe, nuevoUsuario)
+
