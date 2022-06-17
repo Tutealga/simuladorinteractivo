a34a10637f366556fb6ab7267a01d9931c16e6e6
Mateo Alganaras <tutealga@gmail.com>
2022-06-17 01:10:15 -0300

    tercer commit

--- js/main.js
+++ js/main.js
@@ -1,14 +1,40 @@
-let cantidad = prompt('Cuanto dinero quieres pedir prestado: ')
-let cuotas = prompt('En cuantas cuotas quieres devolverlo: ')
-
-function calcular(cantidad, cuotas){
-    calculo = (cantidad + (cantidad * (0.12 * cuotas))) / cuotas
-    total = (cantidad + (cantidad * (0.12 * cuotas)))
-    function devolucionCuotas(cuotas, calculo){
+class Prestamo{
+    constructor(){
+        this.cantidad = 0;
+        this.cuotas = 0; 
+    }
+    setCantidad(value) {this.cantidad = value}
+    setCuotas(cuotas) { this.cuotas = cuotas}
+    calcular(cantidad, cuotas){
+        calculo = (cantidad + (cantidad * (0.12 * cuotas))) / cuotas
+        total = (cantidad + (cantidad * (0.12 * cuotas)))
+    }
+    devolucionCuotas(cuotas, calculo){
         alert('Tendras que devolver ' + cuotas + ' cuotas de ' + calculo)
     }
-    function totalPagar(total){
+    totalPagar(total){
         alert('Tendras que devolver ' + total)
+    }
+    }
+
+    
+class Cuota{
+    constructor(cuotas){
+        this.cuotas = cuotas
     }
 }
 
+const prestamo = new Prestamo()
+prestamo.setCantidad(parseInt(prompt('Cuanto dinero quieres pedir prestado: ')))
+
+const c1 = new Cuota(1)
+const c3 = new Cuota(3)
+const c6 = new Cuota(6)
+const c9 = new Cuota(9)
+const c12 = new Cuota(12)
+
+prestamo.setCuotas(c1)
+prestamo.setCuotas(c3)
+prestamo.setCuotas(c6)
+prestamo.setCuotas(c9)
+prestamo.setCuotas(c12)
