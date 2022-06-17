08bc29b3929febea561204776eb5751641b1035a
Mateo Alganaras <tutealga@gmail.com>
2022-06-17 16:30:29 -0300

    Agregado find para saber si existe las cuotas ingresadas

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 let monto = parseInt(prompt('Cuanto dinero queres pedir prestado: '))
-let cuotas = parseInt(prompt('En cuantas cuotas queres devolver el prestamo (min: 1 - max: 12): '))
+let cuantasCuotas = parseInt(prompt('En cuantas cuotas queres devolver el prestamo (min: 1 - max: 12): '))
+
+const cuotas = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+const existe = cuotas.find((cuota) => {
+    return cuota === cuantasCuotas
+ })
 
 class Prestamo{
     constructor(monto, cuotas){
@@ -8,24 +14,11 @@ class Prestamo{
     }
     calcular(monto, cuotas){
         let resultado = (monto + (monto * cuotas * 0.12)) / cuotas
-        let total = (monto + (monto * (cuotas * 0.12)))
+        let total = monto + (monto * cuotas * 0.12)
         alert('Vas a devolver ' + cuotas + ' cuotas de ' + resultado)
         alert('En total devolveras ' + total)
     }
 }
 
-const prestamo = new Prestamo(monto, cuotas)
-prestamo.calcular(monto, cuotas)
-
-
-
-
-
-
-
-/*calcularIntereses(monto, cuantasCuotas)
-
-const existe = cuotas.find((cuota) => {
-    cuota = cuantasCuotas
-    return cuota
-})*/
+const prestamo = new Prestamo(monto, existe)
+prestamo.calcular(monto, existe)
