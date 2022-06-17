b88775f776fd7b8eef9b503f93653ca408e4371d
Mateo Alganaras <tutealga@gmail.com>
2022-06-16 22:15:13 -0300

    segundo commit

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
-let monto = prompt('Cuanto dinero quieres gastar: ')
-let cuotas = prompt('En cuantas cuotas quieres abonar: ')
+let cantidad = prompt('Cuanto dinero quieres pedir prestado: ')
+let cuotas = prompt('En cuantas cuotas quieres devolverlo: ')
 
-function cuantasCuotas(monto, cuotas){
-    alert('Tendras que abonar ' + cuotas + ' cuotas de ' + monto/cuotas)
-    for (i = monto; i > 0; i -= monto/cuotas){
-        alert('Falta por pagar: ' + i)
-    }
+
+switch(cuotas) {
+    case 1:
+        
+        break
+    case 3:
+        break
+    case 6:
+        break
+    case 9:
+        break
+    case 12:
+        break
+    default:
+        cuotas = prompt('En cuantas cuotas quieres devolverlo: ')
 }
 
 if (cuotas <= 12){
@@ -15,5 +25,27 @@ if (cuotas <= 12){
         cuotas = prompt('Ingrese correctamente las cuotas: ')
     }
     cuantasCuotas(monto, cuotas)
+}
+
+//class User{
+//    constructor(nombre, edad){
+//        this.nombre = nombre;
+//        this.edad = edad;
+//    }
+//}
+
+class Prestamo{
+    constructor(){
+        this.usuario = '';
+        this.cantidad = cantidad;
+        this.cuotas = cuotas; 
+    }
+    setUsuario(value) { this.usuario = value }
+    devolucionCuotas(){
+        alert('Tendras que devolver ' + cuotas + ' cuotas de ' + cantidad/cuotas)
+        for (i = cantidad; i > 0; i -= cantidad/cuotas){
+            alert('Falta por pagar: ' + i)
+        }
+    }
 }
 
