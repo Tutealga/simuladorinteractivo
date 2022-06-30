15f668500e7c9d1f944a0c6a731dca5af2342711
Mateo Alganaras <tutealga@gmail.com>
2022-06-30 20:55:45 -0300

    Storage + JSON

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
-let usuario = prompt('Como es tu nombre: ')
-let monto = parseInt(prompt('Cuanto dinero queres pedir prestado: '))
-let cuantasCuotas = parseInt(prompt('En cuantas cuotas queres devolver el prestamo (min: 1 - max: 12): '))
+let botonPrestamo = document.querySelector('#boton1')                          
+botonPrestamo.onclick = () => {
+            let usuario = document.querySelector("#id1").value;
+            let monto = parseInt(document.querySelector("#id2").value);
+            let cuantasCuotas = parseInt(document.querySelector("#id3").value);
 
-const cuotas = [1,2,3,4,5,6,7,8,9,10,11,12]
+            const cuotas = [1,2,3,4,5,6,7,8,9,10,11,12]
 
-const existe = cuotas.find((cuota) => {
-    return cuota === cuantasCuotas
- })
+            const existe = cuotas.find((cuota) => {
+                return cuota === cuantasCuotas
+                })
+
+            const nuevoUsuario = new Usuario(usuario, monto, existe)
+            const prestamo = new Prestamo(monto, existe, nuevoUsuario)
+
+            prestamo.calcular(monto, existe, nuevoUsuario, prestamo)
+            }   
 
 class Prestamo{
     constructor(monto, cuotas, usuario){
@@ -14,23 +22,21 @@ class Prestamo{
         this.cuotas = cuotas; 
         this.usuario = usuario;
     }
-    calcular(monto, cuotas, usuario){
+    calcular(monto, cuotas, usuario, prestamo){
         let resultado = (monto + (monto * cuotas * 0.12)) / cuotas
         let total = monto + (monto * cuotas * 0.12)
         return prestamo.mostrarInfo(resultado, total, usuario)
     }
     mostrarInfo(resultado, total, usuario){
-        //alert(usuario.nombre + ' tendras que devolver ' + this.cuotas + ' cuotas de ' + resultado)
-        //alert(usuario.nombre + ' en total devolveras ' + total)
         let usuarioDebe = document.querySelector('.card')
         usuarioDebe.innerHTML = `<div class="card-body">
                                   <h5 class="card-title">${usuario.nombre}</h5>
                                   <p id="total" class="card-text">Debe: $${total}</p>
                                   <p id="cuotas" class="card-text">Cuotas: ${this.cuotas} de $${resultado}</p>
-                                  <button type="button" class="btn btn-primary">Pagar</button>
+                                  <button type="button" id="pagar" class="btn btn-primary">Pagar</button>
                                   </div>`
 
-        let boton = document.querySelector('.btn-primary')                          
+        let boton = document.querySelector('#pagar')                          
         boton.onclick = () => {
             let totalDescontado = document.querySelector('#total')
             let cuotasDescontadas = document.querySelector('#cuotas')
@@ -57,9 +63,3 @@ class Usuario{
         this.cuotas = cuotas;
     }
 }
-
-const nuevoUsuario = new Usuario(usuario, monto, existe)
-
-const prestamo = new Prestamo(monto, existe, nuevoUsuario)
-prestamo.calcular(monto, existe, nuevoUsuario)
-
